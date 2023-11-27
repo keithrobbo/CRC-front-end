a92ebc43ae0c1b94c0c4e7bda2ffc62e42e7dbb4
keithrobbo <139442519+keithrobbo@users.noreply.github.com>
2023-11-27 13:04:06 +0000

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-fetch('https://ykpr9675sb.execute-api.eu-west-2.amazonaws.com/default/CRCVisitorCounter') 
-  .then(response => response.json()) 
-  .then(data => document.getElementById("counter").innerHTML);
-  
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("counter").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open(
+  "GET",
+  "https://ykpr9675sb.execute-api.eu-west-2.amazonaws.com/default/CRCVisitorCounter",
+  true
+);
+xhttp.send();  
