282eca618af9b588be94c73eedd439186dfe6f31
keithrobbo <139442519+keithrobbo@users.noreply.github.com>
2023-11-27 11:02:21 +0000

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
-fetch('https://ykpr9675sb.execute-api.eu-west-2.amazonaws.com/default/CRCVisitorCounter');
+fetch('https://ykpr9675sb.execute-api.eu-west-2.amazonaws.com/default/CRCVisitorCounter') 
+  .then(response => response.json()) 
+  .then(data => console.log(data));
